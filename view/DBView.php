f50d75759f4c2c84bd423239f5def8996c149272
Frans Spijkerman <fj.spijkerman@avans.nl>
2019-03-26 09:48:13 +0100

    Introduced class Login (unfortanately it is still all static)

--- view/DBView.php
+++ view/DBView.php
@@ -11,7 +11,7 @@ $view->start('Database');
 <form id="admin" target="database" method="post" action="<?= Secret::DB_ADMIN ?>">
     <input type="hidden" name="pma_servername" value="<?= Secret::DB_HOST ?>" size="24">
     <input type="hidden" name="pma_username" value="<?= Secret::DB_USERNAME ?>" size="24">
-    <input type="hidden" name="pma_password" value="<?= Secret::DB_PASSWORD ?>" size="24">
+    <input type="hidden" name="pma_password" value="" size="24">
     <input type="hidden" name="server" value="1">    
     <p>Of druk op <input value="Start" type="submit"></p>
 </form>
